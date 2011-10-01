22c72b83e2dff25b63966ca1ded4b71f619a5609
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2011-10-01 23:27:40 +0300

    Sign apk to play with it on phone.

---
+++ sign_apk.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd ~/projects/synctab/synctab-android/bin
+
+STOREPASS=`cat ../key/storepass`
+
+cp ../out/production/synctab-android/synctab-android.apk.unsigned ./synctab.apk
+#jarsigner -verbose -keystore ../key/synctab.keystore -storepass "lOOnapArk72#91" synctab.apk khmlabs
+jarsigner -verbose -keystore ../key/synctab.keystore -storepass "$STOREPASS" synctab.apk khmlabs
+/usr/local/android/tools/zipalign -v 4 synctab.apk synctab-aligned.apk
+mv synctab-aligned.apk synctab.apk
