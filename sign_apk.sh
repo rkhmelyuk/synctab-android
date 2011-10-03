d2fbc97d7d05be14584688372561f67db9dfb806
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2011-10-03 23:13:21 +0300

    Released v0.2.0

--- sign_apk.sh
+++ sign_apk.sh
@@ -8,4 +8,5 @@ cp ../out/production/synctab-android/synctab-android.apk.unsigned ./synctab.apk
 #jarsigner -verbose -keystore ../key/synctab.keystore -storepass "lOOnapArk72#91" synctab.apk khmlabs
 jarsigner -verbose -keystore ../key/synctab.keystore -storepass "$STOREPASS" synctab.apk khmlabs
 /usr/local/android/tools/zipalign -v 4 synctab.apk synctab-aligned.apk
-mv synctab-aligned.apk synctab.apk
+rm synctab.apk
+mv synctab-aligned.apk synctab-2.apk
