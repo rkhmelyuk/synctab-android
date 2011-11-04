b7a1e34844f6988cdd65aa74312c27c3595c4dbc
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2011-11-04 16:36:44 +0200

    Index by timestamp.

--- src/com/khmlabs/synctab/db/DbOpenHelper.java
+++ src/com/khmlabs/synctab/db/DbOpenHelper.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.khmlabs.synctab.db.DbMetadata.QueueTasksColumns;
+import com.khmlabs.synctab.db.DbMetadata.SharedTabsColumn;
+import com.khmlabs.synctab.db.DbMetadata.Table;
+
 class DbOpenHelper extends SQLiteOpenHelper {
 
-    private static int DB_VERSION = 4;
+    private static int DB_VERSION = 1;
 
     private static String DB_NAME = "synctab.db";
 
@@ -18,14 +22,26 @@ class DbOpenHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(createSharedTabsTable());
         db.execSQL(createQueueTasksTable());
+
+        db.execSQL(createSharedTabsIndex());
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        if (oldVersion == 2) {
-            db.execSQL(dropSharedTabsTable());
-            db.execSQL(createSharedTabsTable());
-        }
+        // nothing yet
+    }
+
+    private String createSharedTabsIndex() {
+        final StringBuilder builder = new StringBuilder(50);
+
+        // -- index by timestamp to sort faster
+        builder // {
+                .append("create index IX_").append(Table.SHARED_TABS)
+                .append("_TS on ").append(Table.SHARED_TABS)
+                .append("(").append(SharedTabsColumn.TIMESTAMP).append(");");
+        // }
+
+        return builder.toString();
     }
 
     private String createSharedTabsTable() {
@@ -33,12 +49,12 @@ class DbOpenHelper extends SQLiteOpenHelper {
         builder
                 .append("create table ").append(DbMetadata.Table.SHARED_TABS).append("(")
                 .append(DbMetadata.ID).append(" integer primary key,")
-                .append(DbMetadata.SharedTabsColumn.TAB_ID).append(" text unique,")
-                .append(DbMetadata.SharedTabsColumn.LINK).append(" text,")
-                .append(DbMetadata.SharedTabsColumn.TITLE).append(" text,")
-                .append(DbMetadata.SharedTabsColumn.DEVICE).append(" text,")
-                .append(DbMetadata.SharedTabsColumn.FAVICON).append(" text,")
-                .append(DbMetadata.SharedTabsColumn.TIMESTAMP).append(" long);");
+                .append(SharedTabsColumn.TAB_ID).append(" text unique,")
+                .append(SharedTabsColumn.LINK).append(" text,")
+                .append(SharedTabsColumn.TITLE).append(" text,")
+                .append(SharedTabsColumn.DEVICE).append(" text,")
+                .append(SharedTabsColumn.FAVICON).append(" text,")
+                .append(SharedTabsColumn.TIMESTAMP).append(" long);");
 
         return builder.toString();
     }
@@ -48,21 +64,21 @@ class DbOpenHelper extends SQLiteOpenHelper {
         builder
                 .append("create table ").append(DbMetadata.Table.QUEUE_TASKS).append("(")
                 .append(DbMetadata.ID).append(" integer primary key,")
-                .append(DbMetadata.QueueTasksColumns.TYPE).append(" integer,")
-                .append(DbMetadata.QueueTasksColumns.PARAM).append(" text);");
+                .append(QueueTasksColumns.TYPE).append(" integer,")
+                .append(QueueTasksColumns.PARAM).append(" text);");
 
         return builder.toString();
     }
 
     private String dropSharedTabsTable() {
         final StringBuilder builder = new StringBuilder(40);
-        builder.append("drop table ").append(DbMetadata.Table.SHARED_TABS).append(";");
+        builder.append("drop table ").append(Table.SHARED_TABS).append(";");
         return builder.toString();
     }
 
     private String dropQueueTasksTable() {
         final StringBuilder builder = new StringBuilder(40);
-        builder.append("drop table ").append(DbMetadata.Table.QUEUE_TASKS).append(";");
+        builder.append("drop table ").append(Table.QUEUE_TASKS).append(";");
         return builder.toString();
     }
 }
