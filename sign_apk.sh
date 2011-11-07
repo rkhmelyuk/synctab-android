cf78937532a697ea610b1560dc584db47265c75c
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2011-11-07 12:46:49 +0200

    Build script.

--- sign_apk.sh
+++ sign_apk.sh
@@ -5,7 +5,6 @@ cd ~/projects/synctab/synctab-android/bin
 STOREPASS=`cat ../key/storepass`
 
 cp ../out/production/synctab-android/synctab-android.apk.unsigned ./synctab.apk
-#jarsigner -verbose -keystore ../key/synctab.keystore -storepass "lOOnapArk72#91" synctab.apk khmlabs
 jarsigner -verbose -keystore ../key/synctab.keystore -storepass "$STOREPASS" synctab.apk khmlabs
 /usr/local/android/tools/zipalign -v 4 synctab.apk synctab-aligned.apk
 rm synctab.apk
