f38429ccc6cde2c9a9f6dbbfdf0bf69ca63a6f26
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2011-11-23 22:32:01 +0200

    Whew... Way better About form.

--- gen/com/khmlabs/synctab/R.java
+++ gen/com/khmlabs/synctab/R.java
@@ -94,34 +94,31 @@ public final class R {
         public static final int yes=0x7f020018;
     }
     public static final class id {
-        public static final int about=0x7f0d0016;
-        public static final int device=0x7f0d0013;
-        public static final int email=0x7f0d0005;
-        public static final int email_label=0x7f0d0004;
-        public static final int help=0x7f0d0017;
-        public static final int login=0x7f0d0008;
-        public static final int login_label=0x7f0d0003;
-        public static final int logout=0x7f0d001a;
-        public static final int password=0x7f0d0007;
-        public static final int password_label=0x7f0d0006;
-        public static final int refresh=0x7f0d0018;
-        public static final int register=0x7f0d0009;
-        public static final int registration_label=0x7f0d000c;
-        public static final int settings=0x7f0d0019;
-        public static final int share_tab_status_img=0x7f0d000d;
-        public static final int start_help_notice=0x7f0d000b;
-        public static final int sync_tab_status=0x7f0d000e;
-        public static final int tab_date=0x7f0d0012;
-        public static final int tab_icon=0x7f0d0010;
-        public static final int tab_link=0x7f0d0011;
-        public static final int tab_title=0x7f0d000f;
-        public static final int tabs=0x7f0d000a;
-        public static final int titlebar=0x7f0d0014;
-        public static final int titlebar_icon=0x7f0d0015;
-        public static final int user_info=0x7f0d0000;
-        public static final int username=0x7f0d0001;
-        public static final int version=0x7f0d0002;
-        public static final int view_shared_tabs=0x7f0d001b;
+        public static final int about=0x7f0d0013;
+        public static final int device=0x7f0d0010;
+        public static final int email=0x7f0d0002;
+        public static final int email_label=0x7f0d0001;
+        public static final int help=0x7f0d0014;
+        public static final int login=0x7f0d0005;
+        public static final int login_label=0x7f0d0000;
+        public static final int logout=0x7f0d0017;
+        public static final int password=0x7f0d0004;
+        public static final int password_label=0x7f0d0003;
+        public static final int refresh=0x7f0d0015;
+        public static final int register=0x7f0d0006;
+        public static final int registration_label=0x7f0d0009;
+        public static final int settings=0x7f0d0016;
+        public static final int share_tab_status_img=0x7f0d000a;
+        public static final int start_help_notice=0x7f0d0008;
+        public static final int sync_tab_status=0x7f0d000b;
+        public static final int tab_date=0x7f0d000f;
+        public static final int tab_icon=0x7f0d000d;
+        public static final int tab_link=0x7f0d000e;
+        public static final int tab_title=0x7f0d000c;
+        public static final int tabs=0x7f0d0007;
+        public static final int titlebar=0x7f0d0011;
+        public static final int titlebar_icon=0x7f0d0012;
+        public static final int view_shared_tabs=0x7f0d0018;
     }
     public static final class integer {
         public static final int link_max_size=0x7f080000;
@@ -145,19 +142,23 @@ public final class R {
         public static final int minutes=0x7f0a0000;
     }
     public static final class string {
-        public static final int about=0x7f090039;
+        public static final int about=0x7f09003a;
         public static final int about_activity=0x7f09000a;
+        public static final int about_references=0x7f09004f;
         public static final int account_registration=0x7f090021;
         public static final int application_label=0x7f090005;
         public static final int authentication=0x7f090022;
-        public static final int chrome_extension=0x7f09003e;
+        public static final int chrome_extension=0x7f090040;
+        public static final int chrome_extension_summary=0x7f09004b;
         public static final int chrome_extension_url=0x7f090004;
-        public static final int contact=0x7f090040;
+        public static final int contact=0x7f090042;
+        public static final int contact_summary=0x7f09004e;
         public static final int contact_url=0x7f090003;
         public static final int copy_link=0x7f090034;
-        public static final int current_tag=0x7f090046;
-        public static final int current_tag_summary=0x7f090047;
-        public static final int documentation=0x7f09003d;
+        public static final int current_tag=0x7f090048;
+        public static final int current_tag_summary=0x7f090049;
+        public static final int documentation=0x7f09003f;
+        public static final int documentation_summary=0x7f09004c;
         public static final int email=0x7f090019;
         public static final int email_password_required=0x7f090024;
         public static final int enqueue_sync=0x7f09000e;
@@ -177,11 +178,12 @@ public final class R {
         public static final int login_activity=0x7f090008;
         public static final int logout=0x7f09002a;
         public static final int logout_progress=0x7f09002b;
-        public static final int msg_failed_to_remove_tab=0x7f090038;
-        public static final int msg_failed_to_reshared_tab=0x7f090037;
-        public static final int msg_tab_is_removed=0x7f090035;
-        public static final int msg_tab_is_reshared=0x7f090036;
+        public static final int msg_failed_to_remove_tab=0x7f090039;
+        public static final int msg_failed_to_reshared_tab=0x7f090038;
+        public static final int msg_tab_is_removed=0x7f090036;
+        public static final int msg_tab_is_reshared=0x7f090037;
         public static final int no_connection=0x7f090029;
+        public static final int none=0x7f09003c;
         public static final int not_a_user=0x7f09002f;
         public static final int nothing_to_share=0x7f090011;
         public static final int open=0x7f090016;
@@ -191,8 +193,8 @@ public final class R {
         public static final int pref_url_summary=0x7f090015;
         public static final int pref_url_title=0x7f090014;
         public static final int refresh=0x7f09002d;
-        public static final int refresh_interval=0x7f090044;
-        public static final int refresh_interval_summary=0x7f090045;
+        public static final int refresh_interval=0x7f090046;
+        public static final int refresh_interval_summary=0x7f090047;
         public static final int refresh_service=0x7f090013;
         public static final int register=0x7f09001e;
         public static final int register_now=0x7f09001f;
@@ -200,27 +202,28 @@ public final class R {
         public static final int registration_activity=0x7f090009;
         public static final int remove=0x7f090032;
         public static final int reshare=0x7f090031;
-        public static final int send_tab_to=0x7f090042;
+        public static final int send_tab_to=0x7f090044;
         public static final int send_to=0x7f090033;
-        public static final int settings=0x7f090043;
+        public static final int settings=0x7f090045;
         public static final int settings_activity=0x7f09000b;
-        public static final int start_help_notice=0x7f090041;
-        public static final int submit_issue=0x7f09003f;
+        public static final int start_help_notice=0x7f090043;
+        public static final int submit_issue=0x7f090041;
+        public static final int submit_issue_summary=0x7f09004d;
         public static final int submit_issue_url=0x7f090002;
         public static final int success_sync=0x7f09000d;
         public static final int sync_in_progress=0x7f09000c;
         public static final int synctab=0x7f090006;
         public static final int synctab_activity=0x7f090007;
         public static final int synctab_service=0x7f090012;
+        public static final int touch_to_open=0x7f090035;
         public static final int unknown=0x7f090030;
-        public static final int username=0x7f09003a;
-        public static final int version=0x7f09003b;
-        public static final int website=0x7f09003c;
+        public static final int username=0x7f09003b;
+        public static final int version=0x7f09003d;
+        public static final int website=0x7f09003e;
+        public static final int website_summary=0x7f09004a;
         public static final int website_url=0x7f090001;
     }
     public static final class style {
-        public static final int AboutLabel=0x7f0b000b;
-        public static final int AboutValue=0x7f0b000c;
         public static final int ActivityWithBackground=0x7f0b0001;
         public static final int LoginButton=0x7f0b0009;
         public static final int MainButton=0x7f0b0007;
@@ -234,7 +237,7 @@ public final class R {
         public static final int ToolButton=0x7f0b0008;
     }
     public static final class xml {
-        public static final int help=0x7f040000;
+        public static final int about=0x7f040000;
         public static final int prefs=0x7f040001;
     }
 }
--- src/com/khmlabs/synctab/ui/AboutActivity.java
+++ src/com/khmlabs/synctab/ui/AboutActivity.java
@@ -1,37 +1,84 @@
 package com.khmlabs.synctab.ui;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
 
 import com.khmlabs.synctab.R;
 import com.khmlabs.synctab.SyncTabApplication;
 import com.khmlabs.synctab.util.AppHelper;
+import com.khmlabs.synctab.util.IntentHelper;
 
 /**
  * Shows some detail information about user and application.
  */
-public class AboutActivity extends BaseActivity {
+public class AboutActivity extends PreferenceActivity {
+
+    private static final int USERNAME = 0;
+    private static final int VERSION = 1;
+    private static final int LINKS_CATEGORY = 2;
+
+    private static final int[] BROWSABLE_URLS = {
+            R.string.website_url,
+            R.string.chrome_extension_url,
+            R.string.help_url,
+            R.string.submit_issue_url,
+            R.string.contact_url
+    };
+
+    TitleBarHelper titlebarHelper;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.about);
         setContentView(R.layout.activity_about);
 
-        // ---- username if authenticated
-        final SyncTabApplication app = getSyncTabApplication();
+        titlebarHelper = new TitleBarHelper(this);
+
+        initInformation();
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        // add any buttons to title bar
+        titlebarHelper.setup();
+    }
+
+    private void initInformation() {
+        final SyncTabApplication app = (SyncTabApplication) getApplication();
+
+        final PreferenceScreen screen = getPreferenceScreen();
+
+        // -- Username preparation
+
+        Preference username = screen.getPreference(USERNAME);
         if (app.isAuthenticated()) {
-            // set user email as username
-            final TextView username = (TextView) findViewById(R.id.username);
-            username.setText(getSyncTabApplication().getAuthEmail());
-        }
-        else {
-            // hide the panel with user info
-            findViewById(R.id.user_info).setVisibility(View.GONE);
+            username.setSummary(app.getAuthEmail());
         }
 
-        // ---- application version
-        final TextView version = (TextView) findViewById(R.id.version);
-        version.setText(AppHelper.getVersionName(this));
+        // -- Version preparation
+
+        Preference version = screen.getPreference(VERSION);
+        version.setSummary(AppHelper.getVersionName(this));
+
+        // -- Commands
+
+        PreferenceCategory category = (PreferenceCategory) screen.getPreference(LINKS_CATEGORY);
+
+        for (int i = 0; i < BROWSABLE_URLS.length; i++) {
+            final String url = getResources().getString(BROWSABLE_URLS[i]);
+
+            category.getPreference(i).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                public boolean onPreferenceClick(Preference preference) {
+                    IntentHelper.browseLink(AboutActivity.this, url);
+                    return true;
+                }
+            });
+        }
     }
+
 }
